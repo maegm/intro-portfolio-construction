93e8991a60f0059f33548ac49f4f21db7d234c51
mauri1491 <m_gutierrez_m91@hotmail.com>
2023-08-06 17:26:22 -0400

    Ahora es posible cargar Fama-French Dataset para cualquier decil

--- edhec_risk_kit_108.py
+++ edhec_risk_kit_108.py
@@ -5,13 +5,15 @@ import scipy.stats
 github_repo = 'https://raw.githubusercontent.com/maegm/intro-portfolio-construction/master/'
 
 
-def get_ffme_returns():
+def get_ffme_returns(columns=None):
     """
     Load the Fama-French Dataset for the returns of the Top and Bottom Deciles by MarketCap
     """
+    if columns is None:
+        columns = ['Lo 10', 'Hi 10']
     me_m = pd.read_csv(github_repo + "data/Portfolios_Formed_on_ME_monthly_EW.csv",
                        header=0, index_col=0, na_values=-99.99)
-    rets = me_m[['Lo 10', 'Hi 10']]
+    rets = me_m[columns]
     rets.columns = ['SmallCap', 'LargeCap']
     rets = rets / 100
     rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
